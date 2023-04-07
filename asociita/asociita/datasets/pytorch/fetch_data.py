0c9785954372167e8a98ee54be8f87a2e2a3726d
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-08 00:54:01 +0200

    adds fetch_data utility for PyTorch

---
+++ asociita/asociita/datasets/pytorch/fetch_data.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+def load_mnist():
+    training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+    )
+
+    test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+    )
+
+    return test_data
