940302b94d5ff0f18c77f81b03263775b362ceaa
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-08 00:32:07 +0200

    adds .load_model method for the orchestrator

--- asociita/asociita/components/orchestrator/orchestrator.py
+++ asociita/asociita/components/orchestrator/orchestrator.py
@@ -1,11 +1,20 @@
 import types, logging
+from typing import Any
 
 class Orchestrator():
     def __init__(self, settings: dict) -> None:
         """Orchestrator is a central object necessary for performing the simulation.
         It connects the nodes, maintain the knowledge about their state and manages the
-        multithread pool."""
-
+        multithread pool.
+        
+        Parameters
+        ----------
+        settings : dict
+            Dictionary object cotaining all the settings of the orchestrator.
+        
+        Returns
+        ----------
+        None"""
         self.state = 1 # Attribute controlling the state of the object.
                          # 0 - initialized, resting
                          # 1 - initialized, in run-time
@@ -13,3 +22,27 @@ class Orchestrator():
         self.settings = settings # Settings attribute (dict)
 
         self.state = 0
+    
+    def load_model(self, model: Any) -> None:
+        """Loads the global model that will be used as the orchestrator's main model
+        In contrast to the client object, load_model and load_data are separated in the
+        instance of the orchestrator class.
+        
+        Parameters
+        ----------
+        model : Any
+            Compiled or pre-compiled model that will be used by the instance of the class.
+        
+        Returns
+        ----------
+        None"""
+        assert self.state == 0, f"Object {self} is not resting, previous operation is still active."
+        self.state = 1
+        
+        try:
+            self.model = model
+        except:
+            logging.critical("Failed to load the model")
+        
+        if self.model != None:
+            self.state = 0
--- asociita/tests/orchestrator_tests/orchestrator_tests.py
+++ asociita/tests/orchestrator_tests/orchestrator_tests.py
@@ -1,4 +1,5 @@
 from asociita.components.orchestrator.orchestrator import Orchestrator
+from asociita.models.pytorch.mnist import MnistNet
 import unittest
 
 class Orchestrator_tests(unittest.TestCase):
@@ -7,7 +8,21 @@ class Orchestrator_tests(unittest.TestCase):
         settings = dict()
         test_orchestrator = Orchestrator(settings=settings)
         self.assertEqual(0, test_orchestrator.state)
+    
+    
+    def load_model(self):
+        # Creates Orchestrator instance
+        settings = dict()
+        test_orchestrator = Orchestrator(settings=settings)
+        self.assertEqual(0, test_orchestrator.state)
+        
+        # Loads model onto the orchestrator
+        model = MnistNet()
+        test_orchestrator.load_model(model)
+        self.assertIsNotNone(test_orchestrator.model)
+
 
 if __name__ == "__main__":
     tests_inst = Orchestrator_tests()
     tests_inst.init_test()
+    tests_inst.load_model()
