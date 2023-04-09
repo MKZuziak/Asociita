890cf058586a623be0c81fa11a82670fd0db382b
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-09 17:17:41 +0200

    Adds factory methods for data downloader

--- asociita/asociita/datasets/pytorch/fetch_data.py
+++ asociita/asociita/datasets/pytorch/fetch_data.py
@@ -1,21 +1,9 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import logging
+from asociita.datasets.pytorch.load_mnist import load_mnist
 
-def load_mnist():
-    training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-    )
-
-    test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor()
-    )
-
-    return test_data
+def load_dataset(dataset_name: str, settings:dict) -> list[list[tuple], list[tuple]]:
+    if dataset_name == 'mnist':
+        return load_mnist(settings=settings)
+    else:
+        logging.warning("Wrong name of the dataset. Please provide a valid name.")
+       
