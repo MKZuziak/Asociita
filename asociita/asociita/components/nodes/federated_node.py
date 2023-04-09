f56140253d112ed4c6f0f644bf5168ba7862f312
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-09 14:07:41 +0200

    Adds a testing instance for the FederatedNode class

--- asociita/asociita/components/nodes/federated_node.py
+++ asociita/asociita/components/nodes/federated_node.py
@@ -4,14 +4,14 @@ class FederatedNode:
     def __init__(self, 
                  node_id: int,
                  model: Any,
-                 data: list[list[tuple], list[tuple]]) -> None:
+                 dataset: list[list[tuple], list[tuple]]) -> None:
         self.state = 1 # Attribute controlling the state of the object.
                          # 0 - initialized, resting
                          # 1 - initialized, in run-time
         
         self.node_id = node_id
         self.model = self.load_model(model)
-        self.train_data, self.test_data = self.load_data(data)
+        self.train_data, self.test_data = self.load_data(dataset)
 
         self.state = 0
     
@@ -22,4 +22,4 @@ class FederatedNode:
 
     def load_data(self, 
                   data: list[list[tuple], list[tuple]]) -> tuple[list[tuple], list[tuple]]:
-        return data
+        return (data, data)
