52edc3deaa7c5d2186e056ee6a0613eb0bfb4871
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-09 14:00:27 +0200

    Adds a state attribute to the FederatedNode

--- asociita/asociita/components/nodes/federated_node.py
+++ asociita/asociita/components/nodes/federated_node.py
@@ -5,9 +5,15 @@ class FederatedNode:
                  node_id: int,
                  model: Any,
                  data: list[list[tuple], list[tuple]]) -> None:
+        self.state = 1 # Attribute controlling the state of the object.
+                         # 0 - initialized, resting
+                         # 1 - initialized, in run-time
+        
         self.node_id = node_id
         self.model = self.load_model(model)
         self.train_data, self.test_data = self.load_data(data)
+
+        self.state = 0
     
 
     def load_model(self, model: Any) -> Any:
