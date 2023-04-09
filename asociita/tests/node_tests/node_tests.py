9e01308b095e88b763b03f4d62df7619ab054328
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-09 14:07:31 +0200

    Adds testing instance for the FederatedNode class

---
+++ asociita/tests/node_tests/node_tests.py
@@ -0,0 +1,8 @@
+from asociita.components.nodes.federated_node import FederatedNode
+import unittest
+
+class Orchestrator_tests(unittest.TestCase):
+
+    def init_test(self):
+        node_id = 0
+        
