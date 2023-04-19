8d2368ed2fb69ed7af5c6449a66b4a3e2b0972b0
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-04-19 16:44:42 +0200

    Adds universal comments schematic to Fedopt_Orchestrator

--- asociita/asociita/components/orchestrator/fedopt_orchestrator.py
+++ asociita/asociita/components/orchestrator/fedopt_orchestrator.py
@@ -15,14 +15,39 @@ orchestrator_logger = Loggers.orchestrator_logger()
 
 class Fedopt_Orchestrator(Orchestrator):
     def __init__(self, settings: dict) -> None:
+        """Orchestrator is a central object necessary for performing the simulation.
+        It connects the nodes, maintain the knowledge about their state and manages the
+        multithread pool. generic_orchestrator.orchestrator is a parent to all more
+        specific orchestrators.
+        
+        -------------
+        Args
+            settings (dict): dictionary object cotaining all the settings of the orchestrator.
+       -------------
+         Returns
+            None"""
         super().__init__(settings)
     
 
     def train_protocol(self,
                 nodes_data: list[datasets.arrow_dataset.Dataset, 
-                datasets.arrow_dataset.Dataset]):
+                datasets.arrow_dataset.Dataset]) -> None:
+        """"Performs a full federated training according to the initialized
+        settings. The train_protocol of the fedopt.orchestrator.Fedopt_Orchestrator
+        follows a popular FedAvg generalisation, FedOpt. Instead of weights from each
+        clients, it aggregates gradients (understood as a difference between the weights
+        of a model after all t epochs of the local training) and aggregates according to 
+        provided rule.
+
+        -------------
+        Args:
+            nodes_data(list[..., ....]): list containing train set and test set
+                wrapped in a hugging facr arrow_dataset.Dataset containers.
+        -------------
+        Returns:
+            None"""
         
-        # Defining the settings
+        # SETTINGS -> CHANGE IF NEEDED
         iterations = self.settings['iterations']
         nodes_number = self.settings['number_of_nodes']
         local_warm_start = self.settings["local_warm_start"]
@@ -31,20 +56,21 @@ class Fedopt_Orchestrator(Orchestrator):
         save_path = self.settings["save_path"]
         
 
-        # Creating a list containing nodes, i.e. FederatedNode objects.
+        # CREATING FEDERATED NODES
         nodes_green = create_nodes(nodes, self.node_settings) # Exterior method / function, can override in childr.
 
 
-        # Copying the the local model n times or initiating with local warm start.
+        # CREATING LOCAL MODELS (that will be loaded onto nodes)
         model_list = self.model_initialization(nodes_number=nodes_number,
                                                model=self.central_net) # Exterior method / function, can overide in childr.
         
+        # INITIALIZING ALL THE NODES
         nodes_green = self.nodes_initialization(nodes_list=nodes_green, # Exterion method / function, can overide in child.
                                                 model_list=model_list,
                                                 data_list=nodes_data,
                                                 nodes_number=nodes_number)
         
-        #PHASE TWO: MODEL TRAINING
+        # TRAINING PHASE ----- FEDOPT
         with Manager() as manager:
             # create the shared queue
             queue = manager.Queue()
@@ -58,7 +84,7 @@ class Fedopt_Orchestrator(Orchestrator):
                     # Sampling nodes and asynchronously apply the function
                     sampled_nodes = sample_nodes(nodes_green, 
                                                  sample_size=sample_size,
-                                                 orchestrator_logger=orchestrator_logger)
+                                                 orchestrator_logger=orchestrator_logger) # SAMPLING FUNCTION -> CHANGE IF NEEDED
                     results = [pool.apply_async(train_nodes, (node, 'gradients')) for node in sampled_nodes]
                     # consume the results
                     for result in results:
@@ -66,7 +92,7 @@ class Fedopt_Orchestrator(Orchestrator):
                         gradients[node_id] = copy.deepcopy(model_gradients)
                     
                     # Computing the average of gradients
-                    grad_avg = Aggregators.compute_average(gradients)
+                    grad_avg = Aggregators.compute_average(gradients) # AGGREGATING FUNCTION -> CHANGE IF NEEDED
                     updated_weights = Aggregators.add_gradients(self.central_model.get_weights(), grad_avg)
                     
                     
@@ -76,18 +102,19 @@ class Fedopt_Orchestrator(Orchestrator):
                     # Upadting the orchestrator
                     self.central_model.update_weights(updated_weights)
 
-                    # Logging the metrics
+                    # SAVING METRICS <- ONLY IF ENABLED IN SETTINGS
                     Handler.save_model_metrics(iteration=iteration,
                         model = self.central_model,
                         logger = orchestrator_logger,
                         saving_path= save_path,
-                        log_to_screen=True)
+                        log_to_screen=True) # PRESERVING METRICS FUNCTION -> CHANGE IF NEEDED
                     
+                    # LOGGING METRICS <- ONLY IF ENABLED IN SETTINGS
                     # Logging the metrics of sample or all nodes
                     if self.settings['evaluation'] == "full":
                         for node in nodes_green:
                             Handler.log_model_metrics(iteration=iteration,
                                 model = node.model,
-                                logger = orchestrator_logger)
+                                logger = orchestrator_logger) # LOGGING METRICS FUNCTION -> CHANGE IF NEEDED
         
         orchestrator_logger.critical("Training complete")
