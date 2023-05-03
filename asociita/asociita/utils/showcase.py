40ac621fcf913505acce1cf776b6b5b806d75767
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-05-03 16:46:45 +0200

    adds auxiliary functions for saving images

--- asociita/asociita/utils/showcase.py
+++ asociita/asociita/utils/showcase.py
@@ -1,9 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import random
 
 
 def show_image(img):
     img = np.asarray(img)
     plt.imshow(img)
+
+
+def show_random(imgs_orig, imgs_transformed):
+    fig, ax = plt.subplots(2, 5)
+    for position in range(5):
+        random_pos = random.randint(0, len(imgs_orig) - 1)
+        original_img = np.asarray(imgs_orig[random_pos])
+        transformed_img = np.asarray(imgs_transformed[random_pos])
+        ax[0][position].imshow(original_img)
+        ax[1][position].imshow(transformed_img)
+    plt.show()
+
+
+def save_random(imgs_orig, imgs_transformed, transformation: str):
+    fig, ax = plt.subplots(2, 5)
+    for position in range(5):
+        random_pos = random.randint(0, len(imgs_orig) - 1)
+        original_img = np.asarray(imgs_orig[random_pos])
+        transformed_img = np.asarray(imgs_transformed[random_pos])
+        ax[0][position].imshow(original_img)
+        ax[1][position].imshow(transformed_img)
+    title = transformation + '.pdf'
+    plt.savefig(title)
 
